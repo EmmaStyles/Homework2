be395928c49a91199944a07863c1956534f51811
EmmaStyles <e.styles@student.unsw.edu.au>
2020-03-30 16:48:53 +1100

    HW2 (not working)

---
+++ app/src/main/java/com/example/homework2/Restaurant.java
@@ -0,0 +1,66 @@
+package com.example.homework2;
+
+import java.util.ArrayList;
+
+public class Restaurant {
+    private String name;
+    private String cuisine;
+    private String location;
+    private double rating;
+
+    public Restaurant(){}
+
+    public Restaurant(String name, String cuisine, String location, double rating){
+        this.name = name;
+        this.cuisine = cuisine;
+        this.location = location;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine() {
+        this.cuisine = cuisine;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation() {
+        this.location = location;
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public void setRating(){
+        this.rating = rating;
+    }
+
+    public static ArrayList<Restaurant> getRestaurants() {
+        ArrayList<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant("The Char Rotisserie", "Chicken and burgers", "Bondi Road", 4));
+        restaurants.add(new Restaurant("Ninety Nine Thai", "Thai", "Bondi Road", 4.7));
+        restaurants.add(new Restaurant("Tin Pin Bakery", "Bakery", "Bondi Road", 4.8));
+        restaurants.add(new Restaurant("Mojo's Tapas Bar", "Spanish", "Campbell Parade", 4.7));
+        restaurants.add(new Restaurant("Bondi Trattoria", "Italian", "Campbell Parade", 4.5));
+        restaurants.add(new Restaurant("Society Pizza", "Italian", "Curlewis Street", 4.5));
+        restaurants.add(new Restaurant("Milky Lane Bondi", "Burgers", "Curlewis Street", 3.7));
+        restaurants.add(new Restaurant("Sonoma Cafe", "Cafe", "Gould Street", 4));
+        restaurants.add(new Restaurant("Gelato Messina", "Icecream", "Hall Street", 4));
+        restaurants.add(new Restaurant("Anita", "Icecream", "Campbell Parade", 4.8));
+        return restaurants;
+    }
+}
---
+++ app/src/main/java/com/example/homework2/RestaurantAdapter.java
@@ -0,0 +1,57 @@
+package com.example.homework2;
+
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder> { {
+    private ArrayList<Restaurant> mRestaurants;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public static class MyViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView textView;
+            public MyViewHolder(TextView v) {
+                super(v);
+                textView = v;
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public RestaurantAdapter(ArrayList<Restaurant> restaurants) {
+            mRestaurants = restaurants;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public RestaurantAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+            // create a new view
+            TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.my_text_view, parent, false);
+        ...
+            MyViewHolder vh = new MyViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.textView.setText(mDataset[position]);
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+    }
+}
